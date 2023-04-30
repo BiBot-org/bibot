59524b768cb779c9f509b2dfa8b6c018c39da488
pakc918 <45251697+pakc918@users.noreply.github.com>
2023-05-01 02:12:27 +0900

    feat : Create Footer Button

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import style from '@/components/layouts/Footer.module.css'
+import Image from 'next/image'
 
 export default function Footer() {
   return (
     <footer className={style.defaultFooter}>
-      <div>footermenu-icons</div>
+      <div className={style.FooterButtonWrap}>
+        <div className={style.leftButton}>
+          <Image src="/assets/images/icons/homeIcon.svg" width={25} height={25} priority={true} alt={'homeIcon'} />
+        </div>
+        <div className={style.centerButton}>
+          <Image src="/assets/images/icons/robotIcon.svg" width={25} height={25} priority={true} alt={'robotIcon'} />
+        </div>
+        <div className={style.rightButton}>
+          <Image src="/assets/images/icons/mypageIcon.svg" width={25} height={25} priority={true} alt={'mypageIcon'} />
+        </div>
+      </div>
     </footer>
   )
 }
