d6402515627bf7d536e6106b74813b5877c46a93
Jason Ahn <beat1103@gmail.com>
2023-05-01 09:54:40 +0900

    feat: footer fixed and add animation

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,21 +1,36 @@
 import React from 'react'
+import { footerMenu } from '@/datas/footerMenuData'
+import { footerMenuType } from '@/types/footer/footerTypes'
 import style from '@/components/layouts/Footer.module.css'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 export default function Footer() {
   return (
     <footer className={style.defaultFooter}>
-      <div className={style.FooterButtonWrap}>
-        <div className={style.leftButton}>
-          <Image src="/assets/images/icons/homeIcon.svg" width={25} height={25} priority={true} alt={'homeIcon'} />
-        </div>
-        <div className={style.centerButton}>
-          <Image src="/assets/images/icons/robotIcon.svg" width={25} height={25} priority={true} alt={'robotIcon'} />
-        </div>
-        <div className={style.rightButton}>
-          <Image src="/assets/images/icons/mypageIcon.svg" width={25} height={25} priority={true} alt={'mypageIcon'} />
-        </div>
+      <div className={style.footerBtnWrap}>
+        {
+          footerMenu && footerMenu.map((item: footerMenuType) => {
+            return (
+              <FooterButton key={item.id} item={item} />
+            )
+          })
+        }
       </div>
     </footer>
+  )
+}
+
+const FooterButton = ({ item }: { item: footerMenuType }) => {
+
+  const router = useRouter()
+  const handleButton = () => {
+    console.log('handleButton')
+    if(item.link)router.push(item.link)
+  }
+  return (
+    <div className={item.link === router.pathname ? `${style.footerBtn} ${style.active}` : style.footerBtn} onClick={handleButton}>
+      <Image src={item.iconImgUrl} width={25} height={25} priority={true} alt={item.name} />
+    </div>
   )
 }
---
+++ datas/footerMenuData.ts
@@ -0,0 +1,20 @@
+export const footerMenu = [
+  {
+    id: 1,
+    name: "Home",
+    iconImgUrl: "/assets/images/icons/homeIcon.svg",
+    link: "/"
+  },
+  {
+    id: 2,
+    name: "Bibot",
+    iconImgUrl: "/assets/images/icons/robotIcon.svg",
+    link: "/bibot"
+  },
+  {
+    id: 3,
+    name: "MyPage",
+    iconImgUrl: "/assets/images/icons/mypageIcon.svg",
+    link: "/mypage"
+  }
+]
