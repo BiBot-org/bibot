7bc37ce105aee4043d1a24ea7692aeb30310eef2
DESKTOP-4F42IDV\교육생12 <pakc918@naver.com>
2023-05-03 17:40:39 +0900

    feat : 컴포넌트 추가 #S2P-21

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import Layout from "@/components/layouts/layout";
 import { NextPageWithLayout } from "./_app";
+import AnnounceMent from "@/components/widgets/main/AnnounceMent";
+import CategoryNav from "@/components/widgets/main/CategoryNav";
+import ApprovalHistory from "@/components/modal/main/ApprovalHistory";
 
 const Home: NextPageWithLayout = () => {
   return (
     <main>
-      {/* <h1>Home</h1> */}
+      <CategoryNav/>
+      <AnnounceMent />
+      <ApprovalHistory />
     </main>
   )
 }
