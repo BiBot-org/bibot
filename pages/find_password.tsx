443a9f43558d1a7ae74b0710e3f3a6feafe61782
Jason Ahn <beat1103@gmail.com>
2023-05-04 17:43:50 +0900

    feat: fixed eventAction

--- pages/find_password.tsx
+++ pages/find_password.tsx
@@ -1,16 +1,8 @@
-import React, { useState } from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
 import style from 'components/layouts/find_password.module.css';
 
 export default function FindPassword() {
-  const [showModal, setShowModal] = useState(false);
-
-  const handleButtonClick = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
+  const [showModal, setShowModal] = useState<boolean>(false);
 
   return (
     <>
@@ -20,19 +12,19 @@ export default function FindPassword() {
           <input type="text" placeholder='이메일을 입력하세요'></input>
         </div>
         <div className={style.fake_pw_button_wrap}>
-          <button onClick={handleButtonClick}>임시비밀번호 발급</button>
+          <button type='button' onClick={()=>setShowModal(true)}>임시비밀번호 발급</button>
         </div>
       </form>
-      {showModal && <MyComponent showModal={showModal} setShowModal={setShowModal}/>}
+      {showModal && <MyComponent setShowModal={setShowModal}/>}
     </>
   );
 }
-const MyComponent = (props:{ showModal:boolean,setShowModal:React.Dispatch<React.SetStateAction<boolean>> } ) => {
+const MyComponent = (props:{ setShowModal: Dispatch<SetStateAction<boolean>>} ) => {
   return (
     <>
       <div className={style.issued_black_bg}></div>
       <div className={style.issued_modal_wrap}>
-        <div className={style.issued_modal_close}><a href="#" onClick={()=>props.setShowModal(!props.showModal)}>close</a></div>
+        <div className={style.issued_modal_close}><p onClick={()=>props.setShowModal(false)}>close</p></div>
         <div>
           <p>임시 비밀번호가 발급되었습니다.</p>
         </div>
