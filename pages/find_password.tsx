2dcfb9e87697e6736751793e519e363bb49f3275
kimpopo <kimhyoeun94@naver.com>
2023-05-09 14:05:52 +0900

    feat : 로그인화면 및 비밀번호 찾기 화면 구현 #S2P-64

--- pages/find_password.tsx
+++ pages/find_password.tsx
@@ -1,34 +1,22 @@
-import React, { Dispatch, SetStateAction, useState } from 'react';
+import React from 'react'
 import style from 'styles/module.css/find_password.module.css'
 
-export default function FindPassword() {
-  const [showModal, setShowModal] = useState<boolean>(false);
-
+export default function find_password() {
   return (
-    <>
-      <h1 className={style.find_pw}>비밀번호 찾기</h1>
-      <form className={style.find_password_wrap}>
-        <div className={style.fake_pw}>
-          <input type="text" placeholder='이메일을 입력하세요'></input>
-        </div>
-        <div className={style.fake_pw_button_wrap}>
-          <button type='button' onClick={()=>setShowModal(true)}>임시비밀번호 발급</button>
-        </div>
-      </form>
-      {showModal && <MyComponent setShowModal={setShowModal}/>}
-    </>
-  );
-}
-const MyComponent = (props:{ setShowModal: Dispatch<SetStateAction<boolean>>} ) => {
-  return (
-    <>
-      <div className={style.issued_black_bg}></div>
-      <div className={style.issued_modal_wrap}>
-        <div className={style.issued_modal_close}><p onClick={()=>props.setShowModal(false)}>close</p></div>
-        <div>
-          <p>임시 비밀번호가 발급되었습니다.</p>
-        </div>
+    <form className={style.find_password_wrap}>
+      <img src='/assets/images/icons/Back_button.svg' alt='back_btn' className='back_btn' />
+      <h1 className={style.find_password_header}>비밀번호 찾기</h1>
+      <div className={style.find_words}>
+        <p>가입한 이메일 주소로 임시 비밀번호를 알려드립니다.</p>
+        <p>로그인 후 비밀번호를 꼭 변경해주세요.</p>
       </div>
-    </>
-  );
-};
+      <div className={style.login_box}>
+        <input type='text' placeholder='이메일을 입력해주세요'></input>
+        <input type='password' placeholder='임시 비밀번호 입력'></input>
+      </div>
+      <div className={style.select_btn_wrap}>
+        <button className={style.select_btn}>임시 비밀번호 발급</button>
+      </div>
+    </form>
+  )
+}
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
-import style from 'components/layouts/login.module.css'
+import style from 'styles/module.css/login.module.css'
 
-export default function Login
-  () {
+export default function login() {
   return (
-    <>
-      <h1 className={style.hello_bibot}>환영합니다</h1>
-      <form className={style.idpwrap}>
-        <div className={style.idbox}>
-          <input type="text" placeholder='아이디'></input>
-          <input type="password" placeholder='비밀번호'></input>
-        </div>
-        <div className={style.buttonwrap}>
-          <button>로그인</button>
-          <button>비밀번호 찾기</button>
-        </div>
-      </form>
-    </>
+    <form className={style.login_wrap}>
+      <h1 className={style.login_header}>LOGIN</h1>
+      <img src='/assets/images/bibot-pic/Group_logo.svg' alt='Group_logo' className='Group_logo' />
+      <div className={style.login_box}>
+        <input type='text' placeholder='E-mail'></input>
+        <input type='password' placeholder='Password'></input>
+      </div>
+      <div className={style.select_btn_wrap}>
+        <button className={style.select_btn}>로그인</button>
+      </div>
+      <div className={style.find_password_btn_wrap}>
+        <button className={style.find_password_btn}>비밀번호 찾기</button>
+      </div>
+      <div className={style.sign_up_wrap}>
+        <p>회원이 아니신가요?</p>
+        <button className={style.Sign_btn}>Sign Up</button>
+      </div>
+    </form>
   )
 }
+
