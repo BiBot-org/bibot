a4ec0b0d4dee408ee2a8e603e5f96721320d1a07
pakc918 <45251697+pakc918@users.noreply.github.com>
2023-05-17 01:13:27 +0900

    feat : 비밀번호 변경화면 수정 #S2P-88

--- pages/changepwd.tsx
+++ pages/changepwd.tsx
@@ -1,32 +1,56 @@
-import BackNotiLayout from '@/components/layouts/BackNotiLayout'
 import React from 'react'
+import LoginUrlSetLayout from '@/components/layouts/LoginUrlSetLayout'
+import { Button, Container, Input, Row, Spacer } from '@nextui-org/react'
 import style from '@/styles/pages/changepwd.module.css'
-import Separator from '@/components/ui/Separator'
-import Title from '@/components/ui/Title'
 
 export default function changepwd() {
     return (
         <>
-            <Separator gutter={7} />
-            <Title title='비밀번호 변경' size={20} />
-            <Separator gutter={7} />
-            <div className={style.changePwdWrap}>
-                <input type="password" placeholder="현재 비밀번호" maxLength={12}/>
-                <input type="password" placeholder="새로운 비밀번호(8~12자)" maxLength={12}/>
-                <input type="password" placeholder="새로운 비밀번호 확인" maxLength={12}/>
-                <Separator gutter={7} />
-                <button>비밀번호 변경</button>
-            </div>
+            <Spacer y={3} />
+            <main className={style.mainContainer}>
+                <article>
+                    <Container style={{ padding: 0 }}>
+                        <Row>
+                            <Input.Password
+                                bordered
+                                labelPlaceholder="Input your password"
+                                type='password'
+                                width='100%'
+                            />
+                        </Row>
+                        <Spacer y={1.5} />
+                        <Row>
+                            <Input.Password
+                                bordered
+                                labelPlaceholder="Input your password"
+                                type='password'
+                                width='100%'
+                            />
+                        </Row>
+                        <Spacer y={1.5} />
+                        <Row>
+                            <Input.Password
+                                bordered
+                                labelPlaceholder="Input your password"
+                                type='password'
+                                width='100%'
+                            />
+                        </Row>
+                        <Spacer y={3} />
+                        <Row>
+                            <Button style={{ width: '100%' }}>임시 비밀번호 발급</Button>
+                        </Row>
+                    </Container>
+                </article>
+            </main>
         </>
     )
 }
 
 changepwd.getLayout = function getLayout(page: React.ReactNode) {
     return (
-        <>
-            <BackNotiLayout>
-                {page}
-            </BackNotiLayout>
-        </>
+        <LoginUrlSetLayout title='비밀번호 변경'>
+            {page}
+        </LoginUrlSetLayout>
     )
 }
