91147fbbe5b2925ef4487060d6c3e0941e6c764a
DESKTOP-4F42IDV\교육생12 <pakc918@naver.com>
2023-05-18 18:07:32 +0900

    feat : 영수증 등록화면  sementic 수정 #S2P-92

--- pages/receipt_register.tsx
+++ pages/receipt_register.tsx
@@ -1,56 +1,59 @@
 import React from 'react'
-import BackNotiLayout from '@/components/layouts/BackNotiLayout'
 import Separator from '@/components/ui/Separator'
 import Approval from '@/components/widgets/Approval'
 import Image from 'next/image'
 import style from '@/styles/pages/receipt_register.module.css'
 import ReceiptItem from '@/components/ui/receipt_register/ReceiptItem'
+import BackTitleLayout from '@/components/layouts/BackTitleLayout'
 
 export default function receipt_register() {
     return (
         <>
-            <Separator gutter={7} />
-            <Approval />
-            <div className={style.emptyReceiptWrap}>
-                <Image
-                    src='/assets/images/icons/emptyReceipt.svg'
-                    alt='emptyReceipt'
-                    width={200}
-                    height={200}
-                    priority={true}
-                />
-                <input type='file'></input>
-            </div>
-            <Separator gutter={3} />
-            <div className={style.ReceiptOCRWrap}>
-                <div className={style.ReceiptOCRHeader}>
-                    Payment Details
+            <header>
+                <Approval />
+            </header>
+            <main>
+                <div className={style.emptyReceiptWrap}>
+                    <Image
+                        src='/assets/images/icons/emptyReceipt.svg'
+                        alt='emptyReceipt'
+                        width={200}
+                        height={200}
+                        priority={true}
+                    />
+                    <input type='file'></input>
                 </div>
-                <div className={style.ReceiptOCRDetails}>
-                    <ReceiptItem />
-                    <ReceiptItem />
-                    <ReceiptItem />
-                    <ReceiptItem />
-                    <ReceiptItem />
+                <Separator gutter={3} />
+                <div className={style.ReceiptOCRWrap}>
+                    <div className={style.ReceiptOCRHeader}>
+                        Payment Details
+                    </div>
+                    <div className={style.ReceiptOCRDetails}>
+                        <ReceiptItem />
+                        <ReceiptItem />
+                        <ReceiptItem />
+                        <ReceiptItem />
+                        <ReceiptItem />
+                    </div>
+                    <Separator gutter={1} />
+                    <div className={style.categorySel}>
+                        <p>카테고리</p>
+                        <select>
+                            <option value='식비'>식비</option>
+                            <option value='교통비'>교통비</option>
+                            <option value='문화비'>문화비</option>
+                            <option value='유류비'>유류비</option>
+                        </select>
+                    </div>
                 </div>
-                <Separator gutter={1} />
-                <div className={style.categorySel}>
-                    <p>카테고리</p>
-                    <select>
-                        <option value='식비'>식비</option>
-                        <option value='교통비'>교통비</option>
-                        <option value='문화비'>문화비</option>
-                        <option value='유류비'>유류비</option>
-                    </select>
-                </div>
-            </div>
-            <button className={style.submitBtn}>신청</button>
+                <button className={style.submitBtn}>등록 및 신청</button>
+            </main>
         </>
     )
 }
 
 receipt_register.getLayout = function getLayout(page: React.ReactElement) {
     return (
-        <BackNotiLayout>{page}</BackNotiLayout>
+        <BackTitleLayout title='영수증 등록'>{page}</BackTitleLayout>
     )
 }
