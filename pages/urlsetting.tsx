8efb21b1f521506d053a42c9c1851068039c0920
pakc918 <45251697+pakc918@users.noreply.github.com>
2023-05-23 02:20:11 +0900

    feat : urlsetting 모달 디자인 수정 및 링크 연결 #S2P-98

--- pages/urlsetting.tsx
+++ pages/urlsetting.tsx
@@ -1,11 +1,16 @@
 import LoginUrlSetLayout from '@/components/layouts/LoginUrlSetLayout'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import style from '@/styles/pages/urlsetting.module.css'
 import { Button, Container, Input, Row, Spacer } from '@nextui-org/react'
+import TwoBtnModal from '@/components/modal/TwoBtnModal'
 
-export default function urlsetting() {
+export default function Urlsetting() {
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
     return (
+        <>
+        <TwoBtnModal isModal={isModalOpen} modal={setIsModalOpen} text={'Spharos'} link={'/login'} />
         <main className={style.mainContainer}>
             <article>
                 <Image src="/assets/images/bibot-pic/logoText.svg" alt="logo" width={135} height={46} priority />
@@ -16,15 +21,16 @@ export default function urlsetting() {
                     </Row>
                     <Spacer y={1} />
                     <Row justify='center' >
-                        <Button style={{width:'100%'}}  shadow color={'primary'}>등록</Button>
+                        <Button onClick={() => setIsModalOpen(!isModalOpen)} style={{width:'100%'}}  shadow color={'primary'}>등록</Button>
                     </Row>
                 </Container>
             </article>
         </main>
+        </>
     )
 }
 
-urlsetting.getLayout = function getLayout(page: React.ReactNode) {
+Urlsetting.getLayout = function getLayout(page: React.ReactNode) {
     return (
         <LoginUrlSetLayout title='SETTING'>
             {page}
