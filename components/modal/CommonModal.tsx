a79cdede05900c70ce6d060c0de30bb491e0248c
pakc918 <45251697+pakc918@users.noreply.github.com>
2023-05-23 02:53:12 +0900

    feat : 공통 모달 수정 비밀번호찾기 로그인 링크 추가 #S2P-99

--- components/modal/CommonModal.tsx
+++ components/modal/CommonModal.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import style from '@/components/modal/CommonModal.module.css'
 import Image from 'next/image'
-import Separator from '../ui/Separator'
+import { Button, Spacer } from '@nextui-org/react'
+import { useRouter } from 'next/router';
 
 export default function CommonModal(props: { isModal: boolean, modal: React.Dispatch<React.SetStateAction<boolean>>, text: string, positive: boolean }) {
+    const router = useRouter();
+    
+    if (!props.isModal) return null;
+
     return (
         <div className={style.modalBackground}>
             <div className={style.modalWrap}>
@@ -13,19 +18,23 @@ export default function CommonModal(props: { isModal: boolean, modal: React.Disp
                         : <Image src='/assets/images/icons/negativelogo.svg' alt='negativelogoIcon' width={40} height={40} />
                     }
                 </div>
-                <Separator gutter={0.5} />
+                <Spacer y={0.5} />
                 <div className={style.modalContents}>
                     <div className={style.modalText}>
                         <p>{props.text}</p>
                     </div>
                 </div>
                 <div className={style.contentsBtn}>
-                    <button className={style.confirmBtn} style={props.positive ? {backgroundColor : '#40CCC3'} :{backgroundColor : '#C95DEF'} } onClick={() => props.modal(!props.isModal)}>
-                        {props.positive ? 
-                        <Image src='/assets/images/icons/checkicon.svg' alt='checkicon' width={20} height={20} /> 
-                        : <Image src='/assets/images/icons/cancelicon.svg' alt='cancelicon' width={20} height={20} />
-                        }
-                    </button>
+                    <Button
+                        className={style.confirmBtn}
+                        style={props.positive ? { backgroundColor: '#40CCC3' } : { backgroundColor: '#C95DEF' }}
+                        onClick={() => router.push('/login')}
+                        auto
+                        icon={props.positive ?
+                            <Image src='/assets/images/icons/checkicon.svg' alt='checkicon' width={20} height={20} />
+                            : <Image src='/assets/images/icons/cancelicon.svg' alt='cancelicon' width={20} height={20} />}
+                    >
+                    </Button>
                 </div>
             </div>
         </div>
