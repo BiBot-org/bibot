23425798dc436fca0869a6b267fe7f2490a7837b
DESKTOP-4F42IDV\교육생12 <pakc918@naver.com>
2023-05-24 14:05:00 +0900

    feat : 카드 사용내역 카드 내역 더미데이터 #S2P-101

---
+++ datas/dummy/cardUsedData.ts
@@ -0,0 +1,30 @@
+export const cardUsedData = [
+    {
+        id: 1,
+        category: '식비',
+        title: '맥도날드',
+        price: 7000,
+        date: '2023-05-01',
+    },
+    {
+        id: 2,
+        category: '식비',
+        title: '스타벅스',
+        price: 5000,
+        date: '2023-05-01',
+    },
+    {
+        id: 3,
+        category: '유류비',
+        title: 'GS칼텍스',
+        price: 30000,
+        date: '2023-05-01',
+    },
+    {
+        id: 4,
+        category: '비품비',
+        title: '다이소',
+        price: 10000,
+        date: '2023-05-01',
+    },
+]
