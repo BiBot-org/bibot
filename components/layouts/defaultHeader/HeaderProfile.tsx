c45968fead64e55abb7a7ddff9abd268158e0162
DESKTOP-4F42IDV\교육생12 <pakc918@naver.com>
2023-05-25 16:13:55 +0900

    refactor : 이모지, 그리팅 useEffect 의존성 수정 #S2P-108

--- components/layouts/defaultHeader/HeaderProfile.tsx
+++ components/layouts/defaultHeader/HeaderProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import style from '@/components/layouts/defaultHeader/HeaderProfile.module.css'
 import Image from 'next/image'
 import { HeaderUserType } from '@/types/header/headerTypes'
@@ -10,36 +10,36 @@ export default function HeaderProfile() {
   const [emoji, setEmoji] = useState<string>('' as string)
 
 
-  const greetings = [
+  const greetings = useMemo(() => [
     '반가워요',
     '오늘도 화이팅!',
     '오늘도 힘내요!',
     '오늘도 즐거운 하루 되세요!',
-  ]
+  ], [])
 
-  const emojis = [
+  const emojis = useMemo(() => [
     '👋',
     '👍',
     '👏',
     '👌',
-  ]
+  ], [])
 
   useEffect(() => {
-    
+
     const random = Math.floor(Math.random() * greetings.length)
     setGreeting(greetings[random])
     setEmoji(emojis[random])
-   
+
     const user = JSON.parse(localStorage.getItem('user') || '{}')
     setUser(user)
 
-  }, [])
+  }, [emojis, greetings])
 
 
   return (
     <div className={style.headerProfileWrap}>
       <div className={style.headerProfileImgWrap}>
-      <Image src={'/assets/images/icons/robotIcon.svg'} alt="profile" width={20} height={20} />
+        <Image src={'/assets/images/icons/robotIcon.svg'} alt="profile" width={20} height={20} />
       </div>
       <div className={style.headerProfileNotiWrap}>
         <p>{greeting} {emoji}</p>
