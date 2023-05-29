af777532dd49486781cd565fce514d05ef46ef07
DESKTOP-4F42IDV\교육생12 <pakc918@naver.com>
2023-05-29 16:44:35 +0900

    feat : cardImage css 삭제 #S2P-126

--- components/ui/cardusedlist/CardImage.tsx
+++ components/ui/cardusedlist/CardImage.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
-import style from './CardImage.module.css'
 
 export default function CardImage(props: { name: string, cardNumber: string, valid: string }) {
 
-
     return (
         <>
-            <div className={style.cardImageWrap}>
-                <div className={style.cardImage}>
+            <div>
+                <div>
                     <Image
                         src='/assets/images/icons/registedcard.svg'
                         alt='registered-card'
                         width={327}
                         height={200}
+                        priority
+                        style={{ width: '100%', height: '100%'}}
                     />
                 </div>
-                <div className={style.cardInfo}>
+                <div style={{color: '#fff', position: 'absolute', top: '39%', left: '2rem'}}>
                     <p>{props.name}</p>
                     <p>{props.cardNumber}</p>
                     <p>{props.valid}</p>
