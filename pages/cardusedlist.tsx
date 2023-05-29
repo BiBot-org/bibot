9bb522704d39c65b50c01a104dc92bcab5adca9b
DESKTOP-4F42IDV\교육생12 <pakc918@naver.com>
2023-05-29 16:44:54 +0900

    feat : 페이지 컴포넌트 간소화 #S2P-126

--- pages/cardusedlist.tsx
+++ pages/cardusedlist.tsx
@@ -1,76 +1,12 @@
 import React, { ReactNode, useState } from 'react'
-import style from 'styles/pages/cardusedlist.module.css'
-import Image from 'next/image'
-import Slider from 'react-slick'
 import BackTitleLayout from '@/components/layouts/BackTitleLayout'
-import { cardListData } from '@/datas/dummy/cardListData'
-import { useRouter } from 'next/router'
-import CardImage from '@/components/ui/cardusedlist/CardImage'
-import DeleteModal from '@/components/modal/cardUsedList/DeleteModal'
-import UsedList from '@/components/widgets/cardUsedList/UsedList'
-
+import CardSlide from '@/components/pages/carUsedList/CardSlide'
 
 export default function CardUsedList() {
-  const router = useRouter()
-  const [currentIndex, setCurrentIndex] = useState<number>(1)
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
-
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    centerMode: true,
-    focusOnSelect: true,
-    initialSlide: 1,
-    arrows: false,
-    afterChange: (current: number) => setCurrentIndex(current)
-  }
-
-  const handleCardClick = () => {
-    if (currentIndex === 0) {
-      router.push('/cardregister')
-    }
-  }
 
   return (
     <>
-      <main className={style.mainContainer}>
-        <DeleteModal ismodalopen={isModalOpen} handlemodal={setIsModalOpen} />
-        <Slider {...settings}>
-          <div>
-            <Image src='/assets/images/icons/emptycard.svg' alt='empty-card' width={327} height={200} onClick={handleCardClick} />
-            <div className={style.card_empty}>카드를 등록해 주세요.</div>
-          </div>
-          {
-            cardListData && cardListData.map((data) => {
-              return (
-                <div key={data.id} className={style.cardItem}>
-                  <CardImage name={data.cardname} cardNumber={data.cardnumber} valid={data.validthru} />
-                  <div className={style.card_delete_icon} onClick={() => setIsModalOpen(!isModalOpen)}>
-                    <Image
-                      src='/assets/images/icons/cardDeleteIcon.svg'
-                      alt='card-delete-icon'
-                      width={25}
-                      height={25}
-                    />
-                  </div>
-                  <div className={style.card_name}>{data.cardname}</div>
-                </div>
-              )
-            })
-          }
-        </Slider>
-        {
-          currentIndex === 0 ?
-            <div className={style.bibot_error_alarm}>
-              <Image src='/assets/images/bibot-pic/bibot.svg' alt='bibot' width={25} height={25} />
-              <div className={style.error_alarm}>카드를 등록해 주세요.</div>
-            </div>
-            : <UsedList />
-        }
-      </main>
+      <CardSlide />
     </>
   )
 }
