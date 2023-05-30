4bff46946473699b9775c66f50074e0f1d101264
DESKTOP-4F42IDV\교육생12 <pakc918@naver.com>
2023-05-30 19:28:46 +0900

    feat : 카드 사용 내역 아이템 #S2P-128

--- components/ui/cardusedlist/CardUsedItem.tsx
+++ components/ui/cardusedlist/CardUsedItem.tsx
@@ -2,12 +2,42 @@ import React from 'react'
 import style from './CardUsedItem.module.css'
 import { useRouter } from 'next/router'
 
-export default function CardUsedItem(props: { category: string, title:string, price:number, date:string }) {
+export default function CardUsedItem(props: {
+    category: string,
+    title: string,
+    price: number,
+    date: string,
+    submit: boolean
+}) {
     const router = useRouter()
+
+    const handleClick = () => {
+        if (!props.submit) {
+            router.push('/receiptregister');
+        }
+        else {
+            router.push(`/viewreceipt/${1}`);
+        }
+    };
+
+    const backgroundStyle = {
+        backgroundColor: props.submit ? 'lightgray' : 'transparent',
+    };
+
+    const categoryColor:Record<string, string> = {
+        '식비': 'var(--bibot-primary)',
+        '유류비': 'var(--bibot-secondary)',
+        '비품비': '#FFD28E',
+    }
+
+    const categoryBackground = {
+        backgroundColor: categoryColor[props.category] || 'var(--bibot-primary)',
+    }
+
     return (
-        <div className={style.card_used_list} onClick={() => router.push('/receiptregister')}>
+        <div className={style.card_used_list} onClick={handleClick} style={backgroundStyle} >
             <div className={style.usedItemInfo}>
-                <div className={style.category}>
+                <div className={style.category} style={categoryBackground}>
                     <p>{props.category}</p>
                 </div>
                 <div className={style.useInfo}>
