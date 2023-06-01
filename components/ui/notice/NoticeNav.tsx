7f29fa8fd8513d2bc85f24eeda4bc63552cce21e
DESKTOP-4F42IDV\교육생12 <pakc918@naver.com>
2023-06-01 14:32:38 +0900

    feat : 디자인 변경 및 기능 추가 #S2P-49

--- components/ui/notice/NoticeNav.tsx
+++ components/ui/notice/NoticeNav.tsx
@@ -1,17 +1,78 @@
-import React from 'react'
+import { GetNoticeMain, SearchNotice } from '@/service/notice/NoticeService'
+import { SearchNoticeReq } from '@/types/notice/RequestType'
+import { NoticeDTO } from '@/types/notice/types'
+import { Button, Navbar, Text } from '@nextui-org/react'
+import style from './NoticeNav.module.css'
+import router from 'next/router'
+import React, { useState } from 'react'
 
-export default function NoticeNav() {
+export default function NoticeNav(props: { noticeList: NoticeDTO[], setNoticeList: React.Dispatch<React.SetStateAction<NoticeDTO[]>> }) {
+    const [noticeList, setNoticeList] = useState<SearchNoticeReq>({} as SearchNoticeReq)
+    const [type, setType] = useState<string>('ALL')
+
+    const handleAll = () => {
+        GetNoticeMain().then((res) => {
+            props.setNoticeList(res.data)
+            router.push('/notice')
+        })
+        setType('ALL')
+        scrollTo(0, 0)
+    }
+
+    const handleNotice = () => {
+        SearchNotice({
+            ...noticeList,
+            type: 'COMMON',
+        }).then((res) => {
+            props.setNoticeList(res.data.content)
+            console.log(res.data.content)
+        })
+        setType('COMMON')
+        scrollTo(0, 0)
+    }
+
+    const handleSystem = () => {
+        SearchNotice({
+            ...noticeList,
+            type: 'SYSTEM',
+        }).then((res) => {
+            props.setNoticeList(res.data.content)
+            console.log(res.data.content)
+        })
+        setType('SYSTEM')
+        scrollTo(0, 0)
+    }
+
+    const setNoticeType = (classtype: string) => {
+        return (
+            type === classtype ? true : false
+        )
+    }
+
     return (
-        <div>
-            <ul style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                listStyle: 'none'
-            }}>
-                <li>전체</li>
-                <li>공지사항</li>
-                <li>시스템점검</li>
-            </ul>
-        </div>
+        <Navbar isCompact className={style.navbar} isBordered variant="floating">
+            <Navbar.Content variant="highlight-rounded">
+                <Navbar.Link
+                    isActive={setNoticeType('ALL')}
+                    onClick={handleAll}
+                >
+                    전체
+                </Navbar.Link>
+            </Navbar.Content>
+            <Navbar.Content variant="highlight-rounded">
+                <Navbar.Link
+                    isActive={setNoticeType('COMMON')}
+                    onClick={handleNotice}
+                >
+                    공지사항
+                </Navbar.Link>
+                <Navbar.Link
+                    isActive={setNoticeType('SYSTEM')}
+                    onClick={handleSystem}
+                >
+                    시스템점검
+                </Navbar.Link>
+            </Navbar.Content>
+        </Navbar>
     )
 }
