1469063d4b6edae8c244c16ddfa8c4f2b6d1854c
DESKTOP-4F42IDV\교육생12 <pakc918@naver.com>
2023-06-01 15:52:11 +0900

    feat : import 수정 #S2P-49

--- config/config.export.ts
+++ config/config.export.ts
@@ -1,14 +1,12 @@
-import configLocal from "./config.local";
+// import configLocal from "./config.local";
 import configDevelop from "./config.develop";
 
 const Config = () => {
   switch (process.env.RUN_MODE) {
-    case "local":
-      return configLocal;
     case "dev":
       return configDevelop;
     default:
-      return configLocal;
+      return configDevelop;
   }
 };
 
