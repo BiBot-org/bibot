81bc93f14766798eb040aba70b33695159784012
DESKTOP-4F42IDV\교육생12 <pakc918@naver.com>
2023-06-01 14:31:44 +0900

    feat : 메인 공지사항 전체목록 버튼 추가 디자인 수정 #S2P-49

--- components/widgets/main/AnnounceMent.tsx
+++ components/widgets/main/AnnounceMent.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { GetNoticeMain } from "@/service/notice/NoticeService";
 import { NoticeDTO } from "@/types/notice/types";
 import { AnnouncementPanel } from "./Announcement-panel";
+import Link from "next/link";
 
 export default function AnnounceMent() {
   const [noticeList, setNoticeList] = useState<NoticeDTO[]>([]);
@@ -12,12 +13,41 @@ export default function AnnounceMent() {
 
   return (
     <>
+      <div
+        style={{
+          backgroundColor: '#ffffff40',
+          width: '90%',
+          margin: '0 auto',
+          paddingTop: '0.7rem',
+          borderRadius: '10px',
+          height: '20rem',
+          overflowY: 'scroll',
+        }}
+      >
+        <p
+          style={{
+            textAlign: 'right',
+            width: '90%',
+            margin: '0 auto',
+          }}
+        >
+          <Link
+            style={{
+              textDecoration: 'none',
+              color: 'var(--bibot-primary)'
+            }}
+            href={"/notice"}
+          >
+            전체목록
+          </Link>
+        </p>
         {noticeList &&
           noticeList.map((notice) => (
             <>
               <AnnouncementPanel key={`notice : ${notice.id}`} notice={notice} />
             </>
           ))}
+      </div>
     </>
   );
 }
